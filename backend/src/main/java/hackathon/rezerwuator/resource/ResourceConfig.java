c122e84b6d30114c86e8464a43bccd6935a1d808
Kamil Zur <kamil.zur@ingservicespolska.pl>
2019-10-12 17:42:40 +0200

    Repair after merge

--- backend/src/main/java/hackathon/rezerwuator/resource/ResourceConfig.java
+++ backend/src/main/java/hackathon/rezerwuator/resource/ResourceConfig.java
@@ -35,10 +35,10 @@ public class ResourceConfig {
 
         resourceDtos.add(new ResourceDto("Cola",
                 "Z hackatonu",
-                "5449000000286", "5449000000286", 6,"Ambrozja,Smak", false, true));
+                "5449000000286", "5449000000286", 6, Arrays.asList("Ambrozja", "Smak"), false, true));
         resourceDtos.add(new ResourceDto("Dubiel",
                 "Najlepszy tester i QA",
-                "1234567890128", "1234567890128", 6,"Gruby", false, true));
+                "1234567890128", "1234567890128", 6, Arrays.asList("Gruby"), false, true));
         resourceDtos.forEach( resource -> resourceFacade.addResource(resource.getName(), resource.getDescription(), resource.getQr(),resource.getTags()));
 
         return resourceFacade;
