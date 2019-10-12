f3bcbbed2d6724da5ca869b0fe9dbcce241abd17
DQ38YJ <maciej.czernik@ing.com>
2019-10-12 13:12:41 +0200

    bIND TO SERVER RESOURCES

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -5,23 +5,6 @@ import { MatTableDataSource } from '@angular/material/table';
 import { DashboardService } from './dashboard.service';
 import { ResourceModel } from '../../models/resource.model';
 
-export interface UserData {
-    id: string;
-    name: string;
-    progress: string;
-    color: string;
-}
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = [
-    'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-    'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const NAMES: string[] = [
-    'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-    'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
-
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -36,11 +19,6 @@ export class DashboardComponent implements OnInit {
     @ViewChild(MatSort, {static: true}) sort: MatSort;
 
     constructor(private service: DashboardService) {
-        // Create 100 users
-        // const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-        // Assign the data to the data source for the table to render
-        // this.dataSource = new MatTableDataSource(users);
     }
 
     ngOnInit() {
@@ -49,27 +27,12 @@ export class DashboardComponent implements OnInit {
             this.dataSource.paginator = this.paginator;
             this.dataSource.sort = this.sort;
         });
-
     }
 
     applyFilter(filterValue: string) {
         this.dataSource.filter = filterValue.trim().toLowerCase();
-
         if (this.dataSource.paginator) {
             this.dataSource.paginator.firstPage();
         }
     }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-    const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-    return {
-        id: id.toString(),
-        name: name,
-        progress: Math.round(Math.random() * 100).toString(),
-        color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-    };
 }
